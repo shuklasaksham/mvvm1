03a712c7354fa5d50a61b1baffcf11fbe7d22f16
saksham shukla <saksham.shukla@harman.com>
2022-10-04 10:21:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mvvm1/Api.java
@@ -0,0 +1,12 @@
+package com.example.mvvm1;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+    @GET("marvel")
+    Call<List<Results>> getsuperHeroes();
+}
---
+++ app/src/main/java/com/example/mvvm1/Results.java
@@ -0,0 +1,17 @@
+package com.example.mvvm1;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Results {
+    @SerializedName("name")
+    private String superName;
+
+
+    public Results(String name) {
+        this.superName = name;
+    }
+
+    public String getName() {
+        return superName;
+    }
+}
